317a720500bac9ffe40aef7637e8866af50a82f0
Jacob Hummer <jcbhmr@outlook.com>
2023-09-16 17:50:56 -0500

    add comments

--- main.sh
+++ main.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -ex
 
+# https://cli.github.com/manual/gh_auth_setup-git
 export GH_TOKEN=$INPUT_TOKEN
 gh auth setup-git -h "${INPUT_GITHUB_SERVER_URL#*//}"
 
+# https://betterdev.blog/minimal-safe-bash-script-template/
 tmp_dir=$(mktemp -d)
 trap 'rm -rf "$tmp_dir"' SIGINT SIGTERM ERR EXIT
 
+# https://weblog.west-wind.com/posts/2023/Jan/05/Fix-that-damn-Git-Unsafe-Repository
 git config --global --add safe.directory "$tmp_dir"
 
 git clone "$INPUT_GITHUB_SERVER_URL/$INPUT_REPOSITORY.wiki.git" "$tmp_dir"
 
+# Hidden files (like .myfile.txt, .git/, or .gitignore) are NOT copied.
 rm -rf "${tmp_dir:?}"/*
 cp -afv "$INPUT_PATH"/* "$tmp_dir/"
 
