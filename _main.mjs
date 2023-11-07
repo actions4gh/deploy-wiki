672d7e91d406bbe38d7f8a751fa9dcad49e1bdc6
Jacob Hummer <jcbhmr@outlook.com>
2023-11-07 17:08:44 -0600

    Update _main.mjs

--- _main.mjs
+++ _main.mjs
@@ -1,7 +1,7 @@
 import { spawn } from "node:child_process";
 import { once } from "node:events";
 import { join, dirname } from "node:path";
-const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
+const file = join(dirname(process.argv[1]), "main.sh"); // 👈 CHANGE ME!
 const subprocess = spawn("bash", [file], { stdio: "inherit" });
 await once(subprocess, "spawn");
 subprocess.on("exit", (x) => process.exit(x));
