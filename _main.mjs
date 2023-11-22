88d0af7b26620c776e4c2a6e02507cf1a1f5dfb1
Jacob Hummer <jcbhmr@outlook.com>
2023-11-22 14:24:14 -0600

    Update _main.mjs

--- _main.mjs
+++ _main.mjs
@@ -4,8 +4,5 @@ import { fileURLToPath } from "node:url";
 const file = fileURLToPath(import.meta.resolve("./main.sh")); // 👈 CHANGE ME!
 const subprocess = spawn("bash", [file], { stdio: "inherit" });
 const [exitCode, signal] = await once(subprocess, "exit");
-if (signal) {
-  process.kill(process.pid, signal);
-} else {
-  process.exit(exitCode);
-}
+signal && process.kill(process.pid, signal);
+process.exit(exitCode ?? 100);
